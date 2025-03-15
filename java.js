6a56b4c3f7cc9ff790706f0d9204b0c31e31bc10
tufeeqmushtaqnahami <tufeeqmushtaq000@gmail.com>
2025-03-15 12:19:59 +0530

    Completed html and css and half of the javaScript

---
+++ java.js
@@ -0,0 +1,8 @@
+let menuicon = document.querySelector('.#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+
+menuicon.onclick = () =>{
+    menuicon.classList.toggle('bx-x')
+    navbar.classList.toggle('active');
+}
